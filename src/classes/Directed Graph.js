8e282353f0b22b4516b921f203e30aa30284ec40
Matthew Elcock <mattiose@outlook.com>
2018-07-19 11:18:22 +0100

    Renamed createEdge and createVertex to make them more symantically correct.

--- src/classes/Directed Graph.js
+++ src/classes/Directed Graph.js
@@ -5,7 +5,7 @@ class DirectedGraph {
         this.edges = [];
     }
 
-    createVertex(vertex){
+    addVertex(vertex){
         /*
         # vertex should be an object containing a manditory property 'id' and a series of arbitary optional properties. 
         # **N0TE: optional properties should not be used within the class definition, doing this would make them manditory.
@@ -16,7 +16,7 @@ class DirectedGraph {
         this.vertices.push(vertex)
     }
 
-    createEdge(edge){
+    addEdge(edge){
         /*
         # edge should be an array of two integers, where the first int is the origin vertex and the second int is the destination
         # vertex.ac
